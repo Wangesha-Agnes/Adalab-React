88ef7030f31ed940ed61cb50c53bfab01dc436a5
Wangesha-Agnes <wangeshaagnes@gmail.com>
2024-06-20 15:12:57 +0300

    added changes

---
+++ src/Login/index.js
@@ -0,0 +1,35 @@
+import './index.css'
+import { useState } from 'react';
+import { login } from './utils';
+
+const Login = () => {
+
+const [username, setUserName] = useState('');
+const [password, setPassword] = useState('');
+console.log({username})
+console.log({password})
+
+
+const handleLOgin = async(event) => {
+    event.preventDefault();
+    const result = await login({username,password});
+    console.log({ result });
+}
+
+
+    return(
+        <div>
+<form onSubmit={handleLOgin}>
+    <h2>Login</h2>
+    <input placeholder="Enter Username" type="text" onChange={(event)=>setUserName(event.target.value)}/>
+  <br />
+  <input placeholder="Enter password" type="password" onChange={(event) => setPassword(event.target.value)}/>
+  <br/>
+  <button type="submit">Login</button>
+</form>
+
+        </div>
+    )
+}
+
+export default Login;
